f6dd90241ae1c9a7e32d83082f130939f3471b17
08653 <mihailova.tan12@gmail.com>
2025-04-11 18:29:22 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,16 +1,30 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
     // * 1. Начало.
-    // * 2. Находим кнопку входа/регистрации и присваиваем переменной.
+    // * 2. Находим кнопку входа/регистрации и форму, присваиваем переменные.
     // * 3. При клике на кнопку входа/регистрации форма становится видимой:
     // *    3.1. Да: 
     // *        3.1.1. Форма открывается.
     // *    3.2. Нет: продолжаем.
     // * 4. Конец
+});
+    const headerButton = document.querySelector(".menu__button");
+    const popupDialog = document.querySelector(".dialog");
 
-    const intensiveImg = document.querySelector(".menu__item");
-    intensiveImg.addEventListener('mouseenter', () => {
-        console.log("Мышка наведена на кнопку, показываем форму");
-    });
+    if (headerButton && popupDialog)
 
+        console.log("Существует кнопка и форма");
+        headerButton.addEventListener("click", () => {
+            popupDialog.removeAttribute("hidden");
+        });
+    // Закрытие модального окна при клике его вне области
+    window.addEventListener("click", (event) => {
+        if (event.target === popupDialog) {
+            popupDialog.setAttribute("hidden", true);
+        }
+        const closeModalButton = document.querySelector(".popup__close");
+        closeModalButton.addEventListener("click", () => {
+            popupDialog.setAttribute("hidden", true);
+        });
+        
 });

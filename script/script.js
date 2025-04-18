154be1c1ab89fceb9efc67157301a0d42d234cfa
08653 <mihailova.tan12@gmail.com>
2025-04-18 19:19:31 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -7,16 +7,16 @@ document.addEventListener("DOMContentLoaded", () => {
     // *        3.1.1. Форма открывается.
     // *    3.2. Нет: продолжаем.
     // * 4. Конец
-});
+
     const headerButton = document.querySelector(".menu__button");
     const popupDialog = document.querySelector(".dialog");
 
     if (headerButton && popupDialog)
 
         console.log("Существует кнопка и форма");
-        headerButton.addEventListener("click", () => {
-            popupDialog.removeAttribute("hidden");
-        });
+    headerButton.addEventListener("click", () => {
+        popupDialog.removeAttribute("hidden");
+    });
     // Закрытие модального окна при клике его вне области
     window.addEventListener("click", (event) => {
         if (event.target === popupDialog) {
@@ -26,5 +26,64 @@ document.addEventListener("DOMContentLoaded", () => {
         closeModalButton.addEventListener("click", () => {
             popupDialog.setAttribute("hidden", true);
         });
-        
+    });
+    //3.4 Слайдер изображений
+    let currentIndex = 0;
+    const slider = document.querySelectorAll(".reviews__item");
+    const prevButton = document.querySelector(".reviews__left");
+    const nextButton = document.querySelector(".reviews__right");
+    const visibleCards = 3;
+    updateSlider();
+
+    prevButton.addEventListener("click", () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = slider.length - visibleCards;
+        }
+        updateSlider();
+    });
+
+    nextButton.addEventListener("click", () => {
+        if (currentIndex < slider.length - visibleCards) {
+            currentIndex++;
+        }
+        else {
+            currentIndex = 0;
+        }
+        updateSlider();
+    });
+    function updateSlider() {
+        slider.forEach((item, index) => {
+            if (index >= currentIndex && index < currentIndex + visibleCards) {
+                item.style.display = "block";
+            }
+            else {
+                item.style.display = "none";
+            }
+        });
+    };
+//3.4 Формирование массива из частей заголовков
+const TourContainer = document.querySelector(".inspiration");
+
+//  проверяем существует ли элемент TrenersContainer, если он существует то переходим далее
+    if (TourContainer) {
+//далее создаем массив dataTitleTreners, который содержит строки с именами тренеров.(здесь уже пишем те значения, которые надо подставить вместо слова Тренер 1, Тренер 2 и т.д)
+        const dataTitleTour = [
+            "Экстремальные путешествия",
+            "Семейный отдых",
+            "Романтические поездки"
+        ];
+
+//Объявляем переменную titleTreners и сохраняем в нее все элементы на странице с классом treners__subtitle (где должны стоять имена тренеров)
+        const titleTour = TourContainer.querySelectorAll(".inspiration__subtitle");
+
+// Проходим по каждому элементу массива titleTreners с помощью цикла forEach. Внутри функции 2 переменные: item – текущий заголовок, а index — его индекс в массиве.
+titleTour.forEach((item, index) => {
+
+//здесь обновляем значение текущего заголовка (textContent) на новое значение из массива dataTitleCards, используя индекс текущего заголовка.
+            item.textContent = dataTitleTour[index];
+           });
+    }
+
 });
